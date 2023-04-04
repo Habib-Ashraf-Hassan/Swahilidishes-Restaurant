a823b0c968f1af331c4dc9b3a56c95f4d2e55baf
Habib-Ashraf-Hassan <100794988+Habib-Ashraf-Hassan@users.noreply.github.com>
2023-04-05 01:02:05 +0300

    Apply style specific to log in page

--- login.php
+++ login.php
@@ -34,7 +34,7 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     <meta name="author" content="Ashraf Mohammed Hassan">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css_files/style.css" type="text/css">
+    <link rel="stylesheet" href="css_files/style_login.css" type="text/css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inconsolata">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
